42f16e518f69adce48c76d1fc5a2670c8ced0b66
Kei Abukawa <keichan37@gmail.com>
2019-01-31 15:29:57 +0900

    prev next

--- wp-content/themes/igarashitakako/functions.php
+++ wp-content/themes/igarashitakako/functions.php
@@ -4,4 +4,13 @@
 /* アイキャッチ */
 add_theme_support( 'post-thumbnails' );
 
+add_filter( 'previous_posts_link_attributes', 'add_prev_posts_link_class' );
+function add_prev_posts_link_class() {
+  return 'class="prev-link"';
+}
+add_filter( 'next_posts_link_attributes', 'add_next_posts_link_class' );
+function add_next_posts_link_class() {
+  return 'class="next-link"';
+}
+
 ?>
