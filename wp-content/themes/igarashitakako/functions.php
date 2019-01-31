7a19f2a4815c7c7dc5cee3a2d4f5ab13a115945c
Kei Abukawa <keichan37@gmail.com>
2019-01-31 15:33:27 +0900

    prev next

--- wp-content/themes/igarashitakako/functions.php
+++ wp-content/themes/igarashitakako/functions.php
@@ -4,13 +4,13 @@
 /* アイキャッチ */
 add_theme_support( 'post-thumbnails' );
 
-add_filter( 'previous_posts_link_attributes', 'add_prev_posts_link_class' );
-function add_prev_posts_link_class() {
-  return 'class="prev-link"';
+add_filter( 'previous_post_link', 'add_prev_post_link_class' );
+function add_prev_post_link_class($output) {
+  return str_replace('<a href=', '<a class="prev-link" href=', $output);
 }
-add_filter( 'next_posts_link_attributes', 'add_next_posts_link_class' );
-function add_next_posts_link_class() {
-  return 'class="next-link"';
+add_filter( 'next_post_link', 'add_next_post_link_class' );
+function add_next_post_link_class($output) {
+  return str_replace('<a href=', '<a class="next-link" href=', $output);
 }
 
 ?>
