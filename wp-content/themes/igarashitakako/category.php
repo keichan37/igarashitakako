d9df831f384ff2984f1df982f5b5d27a1bcf84a4
Kei Abukawa <keichan37@gmail.com>
2019-02-01 16:13:21 +0900

    partials

--- wp-content/themes/igarashitakako/category.php
+++ wp-content/themes/igarashitakako/category.php
@@ -1,30 +1,10 @@
 <?php get_header(); ?>
   <div id="top">
     <div class="container">
+      <?php single_cat_title(); ?></h1>
       <?php while (have_posts()) : the_post(); ?>
-        <?php
-          $thumbnail_id = get_post_thumbnail_id();
-          $thumbnail_url = wp_get_attachment_image_src($thumbnail_id,'medium', true);
-        ?>
-        <a class="grid" href="<?php the_permalink(); ?>">
-          <?php if (has_post_thumbnail()): ?>
-            <img class="grid-eyecatch" src="<?php echo $thumbnail_url[0]; ?>" />
-          <?php else: ?>
-            <img class="grid-eyecatch" src="<?php echo get_template_directory_uri(); ?>/images/sample.jpg" />
-          <?php endif; ?>
-
-          <h5 class="grid-title"><?php the_title(); ?></h5>
-          <ul class="grid-category">
-            <?php
-              $cats = get_the_category();
-              foreach($cats as $cat):
-              if($cat->parent) echo '<li class="class_' . $cat->slug . '">' . $cat->cat_name . '</li>';
-              endforeach;
-            ?>
-          </ul>
-        </a>
-
-        <?php endwhile; ?>
+        <?php get_template_part('partials/grid'); ?>
+      <?php endwhile; ?>
         <div class="clearfix"></div>
       <?php wp_reset_query();?>
     </div>
--- wp-content/themes/igarashitakako/front-page.php
+++ wp-content/themes/igarashitakako/front-page.php
@@ -6,34 +6,14 @@
           'paged' => $paged,
           'post_type' => 'post',
           'posts_per_page' => -1,
+          'category_name' => 'portfolio',
           'post_status' => 'publish',
           'has_password' => false,
-        ); ?>
+      );?>
       <?php query_posts( $args ); ?>
       <?php while (have_posts()) : the_post(); ?>
-        <?php
-          $thumbnail_id = get_post_thumbnail_id();
-          $thumbnail_url = wp_get_attachment_image_src($thumbnail_id,'medium', true);
-        ?>
-        <a class="grid" href="<?php the_permalink(); ?>">
-          <?php if (has_post_thumbnail()): ?>
-            <img class="grid-eyecatch" src="<?php echo $thumbnail_url[0]; ?>" />
-          <?php else: ?>
-            <img class="grid-eyecatch" src="<?php echo get_template_directory_uri(); ?>/images/sample.jpg" />
-          <?php endif; ?>
-
-          <h5 class="grid-title"><?php the_title(); ?></h5>
-          <ul class="grid-category">
-            <?php
-              $cats = get_the_category();
-              foreach($cats as $cat):
-              if($cat->parent) echo '<li class="class_' . $cat->slug . '">' . $cat->cat_name . '</li>';
-              endforeach;
-            ?>
-          </ul>
-        </a>
-
-        <?php endwhile; ?>
+        <?php get_template_part('partials/grid'); ?>
+      <?php endwhile; ?>
         <div class="clearfix"></div>
       <?php wp_reset_query();?>
     </div>
